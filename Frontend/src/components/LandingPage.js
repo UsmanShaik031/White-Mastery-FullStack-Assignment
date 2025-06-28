c552060131f22c9820b183cbc772f3f22c565860
UsmanShaik031 <usmanshaiik2003@gmail.com>
2025-06-28 06:16:23 +0530

    frontend

--- Frontend/src/components/LandingPage.js
+++ Frontend/src/components/LandingPage.js
@@ -16,7 +16,7 @@ function LandingPage() {
 					<p className="w3-large w3-center">Most web applications have flow like this.</p>
 			</div>
 			<div className="w3-display-bottomleft w3-padding-large">
-				Developed by <a href="https://github.com/umair-khanzada" target="_blank">umair-khanzada</a>
+				
 			</div>
 		</div>
 	)
